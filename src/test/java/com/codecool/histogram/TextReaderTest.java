c6718fb3da525adab9359fc9251aad7225a5f7e0
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-25 18:57:28 +0100

    Point 1,2. Test resources. Text reader test.

--- src/test/java/com/codecool/histogram/TextReaderTest.java
+++ src/test/java/com/codecool/histogram/TextReaderTest.java
@@ -8,5 +8,34 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TextReaderTest {
+
+    private TextReader textReader;
+
+    @Test
+    public void read_wrongFileName_throwsFileNotFoundException() {
+        textReader = new TextReader("src/test/resources/nonExistingFile.txt");
+        assertThrows(FileNotFoundException.class, () -> textReader.read());
+    }
+
+    @Test
+    public void read_existingButEmptyFile_returnsEmptyString() throws IOException {
+        textReader = new TextReader("src/test/resources/empty.txt");
+        assertEquals("", textReader.read());
+    }
+
+    @Test
+    public void read_oneLineTextInTextFile_returnsStringWithText() throws IOException {
+        textReader = new TextReader("src/test/resources/test.txt");
+        assertEquals("Harry Potter and the Sorcerer's Stone" + System.lineSeparator(), textReader.read());
+    }
+
+    @Test
+    public void read_multipleLinesInTextFile_returnsStringWithText() throws IOException {
+        textReader = new TextReader("src/test/resources/text.txt");
+        assertEquals(33, textReader.read().split(System.lineSeparator()).length);
+        assertEquals("SORTING HAT SONG", textReader.read().split(System.lineSeparator())[0]);
+        assertEquals("Sweet Hufflepuff from valley broad", textReader.read().split(System.lineSeparator())[6]);
+        assertEquals("Unite all the houses and we'll fight as one.", textReader.read().split(System.lineSeparator())[32]);
+    }
     
 }
